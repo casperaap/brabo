95d94e5d042071ea5cb35b26a089ff09218afa06
MrApers <Casper.Apers@Gmail.com>
2025-06-15 21:06:25 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
   <h1 className="font-semibold text-2xl sm:text-3xl text-center">
-    Be a part of&nbsp;
+    Wees een deel van&nbsp;
     <span className="inline-block px-1 bg-[#E30713] text-white">
       Brabo&nbsp;Media
     </span>
@@ -15,7 +15,7 @@ export default function Home() {
 
   {/* sub‑text */}
   <p className="font-semibold text-sm sm:text-base text-center max-w-md">
-    Every voice counts, every contribution is appreciated.
+    Iedereen telt, alle contributies worden geapprecieerd.
   </p>
         <Navbar/>
         <CardForm/>
