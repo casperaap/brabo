1ce63ea7ac9fa7fa2fa2e196d12d3cf9b148cec3
Your Name <you@example.com>
2025-06-17 01:34:35 +0200

    Added PDF to public folder and updated Navbar link

--- components/header.js
+++ components/header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 export default function Navbar() {
-    return (
-      <header className="fixed top-0 left-0 right-0 z-50">
-        <div className="navbar bg-[#070707CC] shadow-sm">
-          <a href="https://www.braboswim.be/" className="btn btn-ghost text-xl">Brabo Community</a>
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50">
+      <div className="navbar bg-[#070707CC] shadow-sm">
+        <a
+          href="/homepage.png"
+          className="btn btn-ghost text-xl"
+        >
+          Brabo Community
+        </a>
+      </div>
+    </header>
+  );
+}
